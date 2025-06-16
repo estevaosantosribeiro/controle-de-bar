de8c9715775bd9dc38af43de2aeef7255427fee1
estevaosantosribeiro <estevaosantosribeiro@gmail.com>
2025-06-16 12:56:45 -0300

    Implementado módulo de contas e validações

--- ControleDeBar.Dominio/ModuloMesa/Mesa.cs
+++ ControleDeBar.Dominio/ModuloMesa/Mesa.cs
@@ -6,14 +6,26 @@ public class Mesa : EntidadeBase<Mesa>
 {
     public int Numero { get; set; }
     public int Capacidade { get; set; }
+    public bool EstaOcupada { get; set; }
 
     public Mesa() { }
 
-    public Mesa(int numero, int capacidade) : this()
+    public Mesa(int numero, int quantidadeDeAssentos) : this()
     {
         Id = Guid.NewGuid();
         Numero = numero;
-        Capacidade = capacidade;
+        Capacidade = quantidadeDeAssentos;
+        EstaOcupada = false;
+    }
+
+    public void Ocupar()
+    {
+        EstaOcupada = true;
+    }
+
+    public void Desocupar()
+    {
+        EstaOcupada = false;
     }
 
     public override void AtualizarRegistro(Mesa registroEditado)
